ff97b942ced144a733b2ee6995700e3494d23a7d
Suraj Singh <playerking2244@gmail.com>
2024-05-08 16:13:10 +0530

    multer middleware  commit

---
+++ src/utils/claudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+
+const uploadOnCloudinary = async (localurlpath) => {
+        try {
+            if(!localurlpath) return null
+
+            // upload file on the cloudinary
+            const res = await cloudinary.uploader.upload(localurlpath, {
+                resource_type : 'auto'
+            })
+            // file uploaded successfully..
+            console.log('file uploaded on cloudinary..', res.url);
+                return res
+        } catch (error) {
+                fs.unlinkSync(localurlpath)  // remove the locally saved temporary file as the upload operations  got falied
+                return null
+        }
+}
+
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
