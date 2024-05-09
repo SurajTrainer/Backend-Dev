c795b432e202d641b7efef506c92a42072f805a6
Suraj Singh <playerking2244@gmail.com>
2024-05-09 14:24:51 +0530

    adding User Registration

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,6 +2,9 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import User from '../models/user.models.js'
+import uploadOnCloudinary from "../utils/claudinary.js";
+import ApiResponse from "../utils/ApiResponse.js";
+
 
 const registerUser = asyncHandler( async (req, res) => {
        
@@ -38,7 +41,39 @@ const registerUser = asyncHandler( async (req, res) => {
 
         const avatarlocalPath = req.files?.avatar[0]?.path;
         const coverImagelocalPath = req.files?.coverImage[0]?.path;
+
+        if (!avatarlocalPath) {
+            throw new ApiError(400 , " Avatar is very important")
+        }
+        
+        const avatar  = await uploadOnCloudinary(avatarlocalPath)
+        const coverImage  = await uploadOnCloudinary(coverImagelocalPath)
+        
+        if (!avatar) {
+            throw new ApiError(400 , " Avatar is very important")
+        }
+
+      const user = await  User.create({
+            fullName,
+            avatar : avatar.url,
+            coverImage : coverImage?.url || '',
+            email ,
+            password,
+            username : username.toLowerCase()
+        })
+
+        const createdUser = await User.findById(user._id).select(
+            "-password -refreshToken"
+        )
+        if (!createdUser) {
+            throw new ApiError(500 , 'something went wrong when you are trying to reagister')
+        }
+
+        return res.status(201).json(
+            new ApiResponse(200 , createdUser , " User Registration successfully")
+        )
 })
+
 
 
 export default registerUser
--- src/utils/claudinary.js
+++ src/utils/claudinary.js
@@ -29,3 +29,6 @@ const uploadOnCloudinary = async (localurlpath) => {
 cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
   { public_id: "olympic_flag" }, 
   function(error, result) {console.log(result); });
+
+
+  export default uploadOnCloudinary
