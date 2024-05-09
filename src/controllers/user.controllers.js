a536015fbb761119b0d252fee88da92333ce4fa7
Suraj Singh <playerking2244@gmail.com>
2024-05-09 13:42:47 +0530

    steps commit

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,31 @@
 
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-        res.status(200).json({
-        message : "ok and well done ho gya.."
-    })
+       
+        // get user details from frontend
+        // validation - should not empty
+        // check if user already exist - username , email
+        // check for images
+        // check for avatar
+        // upload them to cloudinary , avatar
+        // create user object - create entry in db
+        // remove password and token field from response
+        // check for user creation
+        // return res
+
+        const {fullName , email, username, password} = req.body
+        // console.log(username);
+
+        // if (username === '') {
+        //     throw new ApiError(400 , "FullName is required..")
+        // }
+
+            // second methods
+        if ([fullName , email , username , password].some((field) => field?.trim() === "")) {
+            throw new ApiError(400 , "All Fields are required..")
+        }
 })
 
 
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -13,7 +13,7 @@ class ApiError extends Error {
         this.errors = errors
 
         if (stacks) {
-            this.stack = stacks
+            this.stacks = stacks
         }else {
             Error.captureStackTrace(this , this.constructor)
         }
